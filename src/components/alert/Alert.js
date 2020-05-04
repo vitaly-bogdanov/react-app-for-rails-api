7e43790f0d077d9eed3df006ecf0f872f4dc1fb6
Vitaly <kaliummati@gmail.com>
2020-05-04 14:48:23 +0900

    добавлена библиотека PropTypes и задействованна в компоненте Alert

--- src/components/alert/Alert.js
+++ src/components/alert/Alert.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Alert = props => {
+const Alert = props => (
+  <div className={`alert alert-${props.type}`}>
+    <ul>
+      {
+        props.errors.map((error, key) => <li key={key}>{error}</li>)
+      }
+    </ul>
+  </div>
+);
 
-  return (
-    <div className={`alert alert-${props.type}`}>
-      <ul>
-        {
-          props.errors.map((error, key) => <li key={key}>{error}</li>)
-        }
-      </ul>
-    </div>
-  );
-}
+Alert.propTypes = {
+  type: PropTypes.string.isRequired,
+  errors: PropTypes.arrayOf(PropTypes.array).isRequired,
+};
 
 export default Alert;
