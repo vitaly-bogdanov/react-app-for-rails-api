ff67762861c384547701b493efec649159f42df9
Vitaly <kaliummati@gmail.com>
2020-05-17 23:47:21 +0900

    небольшие правки в стилях и верстке

--- src/components/preloader/Preloader.js
+++ src/components/preloader/Preloader.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
-const Preloader = () => <div className="lds-circle"><div></div></div>;
+const Preloader = () => {
+  return (
+    <div className='lds-circle'>
+      <div></div>
+    </div>
+  );
+}
+
+
 
 export default Preloader;
